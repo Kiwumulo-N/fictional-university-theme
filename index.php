c40deada679018038941b0504ce4ae9c3b4c9bd2
Kiwumulo-N <nicksonkiwumulo22@gmail.com>
2023-07-31 18:09:19 +0300

    created three posts and invoked them in my new theme using the famous while loop

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+while(have_posts()){
+the_post(); ?>
+<h2><?php  the_title()?></h2>
+
+
+
+
+
+<?php
+}
+
+
+/*<h1><?php the_title() ?></h1>
+<p><?php the_content() ?></p>*/
+?>
+
+
+
+
+
+
+
